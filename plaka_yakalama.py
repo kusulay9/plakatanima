5273aec6c96b7a0d7b249cc47d856a33e1c931c1
Mehmet Faruk Atalay <33689073+kusulay9@users.noreply.github.com>
2022-01-22 23:01:36 +0300

    Plaka Tanıma
    Bağlantılı bileşen analizi modülünün oluşturulması

--- plaka_yakalama.py
+++ plaka_yakalama.py
@@ -5,7 +5,7 @@ import numpy as np
 
 resim_adresler = os.listdir("veriseti")
 
-img = cv2.imread("veriseti/"+resim_adresler[0])
+img = cv2.imread("veriseti/"+resim_adresler[6])
 img = cv2.resize(img,(500,500))
 
 plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
@@ -31,3 +31,51 @@ kenarlik = cv2.dilate(kenarlik,np.ones((3,3),np.uint8),iterations=1)
 
 plt.imshow(kenarlik,cmap="gray")
 plt.show()
+
+cnt = cv2.findContours(kenarlik,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cnt = cnt[0]
+cnt = sorted(cnt,key=cv2.contourArea,reverse=True)
+
+H,W = 500,500
+plaka = None
+
+for c in cnt:
+    rect = cv2.minAreaRect(c)
+    (x,y),(w,h),r = rect
+    if(w>h and w>h*2) or (h>w and h>w*2):
+       box = cv2.boxPoints(rect)
+       box = np.int64(box)
+
+       minx = np.min(box[:,0])
+       miny = np.min(box[:,1])
+       maxx = np.max(box[:,0])
+       maxy = np.max(box[:,1])
+
+       muh_plaka = img_gray[miny:maxy,minx:maxx].copy()
+       muh_medyan = np.median(muh_plaka)
+
+
+       kon1 = muh_medyan>100 and muh_medyan<200
+       kon2 = h>80 and w<200
+       kon3 = w>80 and h<200
+
+       print(f"muh_plaka medyan{muh_medyan} genişlik:{w} yükseklik:{h}")
+       plt.figure()
+       kon = False
+       if(kon1 and (kon2 or kon3)):
+          cv2.drawContours(img,[box],0,(0,255,0),2)
+          plaka = [minx,miny,w,h]
+
+        
+          plt.title("Plaka Tespit Edildi!!!")
+          kon = True
+       else:
+            cv2.drawContours(img,[box],0,(0,0,255),2)
+            plt.title("Plaka Tespit Edilemedi!!!")
+            plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+            plt.show()
+            if(kon):
+                break
+        
+
+
